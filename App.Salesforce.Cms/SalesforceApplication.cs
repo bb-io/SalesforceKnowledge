ac63be28409d70b22ae2c2204437f69d115b9fef
bZverok <137277669+bZverok@users.noreply.github.com>
2024-04-16 18:49:46 +0300

    Categories (#8)
    * CI: update workflow to invoke DEV deployment
    
    * CI: update pipeline to include dispatch event
    
    * CI: invoke Dev workflow once manually triggered
    
    * added categories
    
    * Update Apps.Salesforce.Cms.csproj
    
    ---------
    
    Co-authored-by: Ihor Kriatov <ihor.kriatov@blackbird.io>

--- App.Salesforce.Cms/SalesforceApplication.cs
+++ App.Salesforce.Cms/SalesforceApplication.cs
@@ -1,13 +1,20 @@
 ﻿using App.Salesforce.Cms.Auth.OAuth2;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Authentication.OAuth2;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace App.Salesforce.Cms;
 
-public class SalesforceApplication : IApplication
+public class SalesforceApplication : IApplication, ICategoryProvider
 {
     private readonly Dictionary<Type, object> _container;
 
+    public IEnumerable<ApplicationCategory> Categories
+    {
+        get => [ApplicationCategory.Cms, ApplicationCategory.CustomerSupport];
+        set { }
+    }
+    
     public string Name
     {
         get => "Salesforce";
