af9e6b5f27c0bd17171192323d41a6d77f5192a1
RiabushenkoA <ariabushenko@blackbird.io>
2025-09-17 20:52:11 +0300

    Added error wrapper for action 'Translate knowledge article from HTML file'

---
+++ App.Salesforce.Cms/Utils/ErrorHandler.cs
@@ -0,0 +1,32 @@
+﻿using Blackbird.Applications.Sdk.Common.Exceptions;
+
+namespace Apps.Salesforce.Cms.Utils
+{
+    public static class ErrorHandler
+    {
+
+        public static async Task<T> ExecuteWithErrorHandling<T>(Func<Task<T>> func)
+        {
+            try
+            {
+                return await func.Invoke();
+            }
+            catch (Exception e)
+            {
+                throw new PluginApplicationException(e.Message);
+            }
+        }
+
+        public static async Task ExecuteWithErrorHandling(Func<Task> func)
+        {
+            try
+            {
+                await func.Invoke();
+            }
+            catch (Exception e)
+            {
+                throw new PluginApplicationException(e.Message);
+            }
+        }
+    }
+}
